88f4dbcb16d89685a89ce4cdd2cd434f4d72c951
Nikhil <nikhilambure25@gmailcom>
2024-07-25 09:11:06 +0530

    first

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('posts/', views.posts, name='posts'),
+    path('post/', views.post, name='post'),
+    path('profile/', views.profile, name='profile')
+]
