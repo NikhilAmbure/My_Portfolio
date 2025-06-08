2e2dcecbf8256cc6698114c677e693c88eb64994
Nikhil Ambure <nikhilambure25@gmail.com>
2025-06-08 23:23:29 +0530

    Updated

--- myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import FormSubmit
-
-class PostForm(forms.ModelForm):
-    class Meta:
-        model = FormSubmit
-        fields = ['name', 'subject', 'email', 'desc']
